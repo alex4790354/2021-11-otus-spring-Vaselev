cd8481473cb1930b404af64ce0d65347c140e6ca
Alexander Vasiliev <9720560@gmail.com>
2021-12-07 16:09:31 +0300

    Commit changes before switch to annotations

--- 01_student_test/src/main/java/ru/otus/spring/Main.java
+++ 01_student_test/src/main/java/ru/otus/spring/Main.java
@@ -1,14 +1,11 @@
 package ru.otus.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.spring.dao.QuestionsDao;
-import ru.otus.spring.dao.QuestionsFromResFile;
 import ru.otus.spring.domain.Question;
 import ru.otus.spring.service.QuestionsService;
-
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
 
