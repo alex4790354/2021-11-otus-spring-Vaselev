98a3b73177a9cf1c4877d9f17c92a73e6f25c810
Alex4790354 <9720560@gmail.com>
2022-01-02 21:05:24 +0300

    remove IOException

--- 05_Spring_Shell/src/main/java/ru/otus/spring/Application.java
+++ 05_Spring_Shell/src/main/java/ru/otus/spring/Application.java
@@ -6,13 +6,10 @@ import org.springframework.context.ApplicationContext;
 import ru.otus.spring.service.interfaces.QuestionsService;
 
 
-import java.io.IOException;
-
-
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
 		QuestionsService questionsServiceCsv = context.getBean(QuestionsService.class);
 		questionsServiceCsv.takeExam();
