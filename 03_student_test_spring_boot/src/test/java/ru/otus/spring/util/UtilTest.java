a91e0def9aac8a388ce215bdaf01f8b89100dd3f
Alex4790354 <9720560@gmail.com>
2022-01-03 00:13:04 +0300

    Change testing

--- 03_student_test_spring_boot/src/test/java/ru/otus/spring/util/UtilTest.java
+++ 03_student_test_spring_boot/src/test/java/ru/otus/spring/util/UtilTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import ru.otus.spring.config.ExamConfig;
+import ru.otus.spring.service.LocalizationFromExamFile;
 import ru.otus.spring.service.interfaces.Localization;
 import ru.otus.spring.service.interfaces.QuestionsService;
 
@@ -28,17 +29,17 @@ class UtilTest {
     private ExamConfig examConfig;
 
     @Autowired
-    private Localization localization;
+    private LocalizationFromExamFile localization;
 
     private static final String EXAM_ASK_NAME_EN = "what is your name?";
-    private static final String EXAM_WELCOME_EN = "Welcome to the chemistry test";
+    private static final String EXAM_WELCOME_EN = "Welcome to the chemistry test.";
     private static final String EXAM_PASS_EN = "Congratulations, you passed the test.";
     private static final String EXAM_FAIL_EN = "You fail the test today, but you can retake it later.";
 
-    private static final String EXAM_ASK_NAME_RU = "Russian: what is your name?";
-    private static final String EXAM_WELCOME_RU = "Russian: Welcome to the chemistry test";
-    private static final String EXAM_PASS_RU = "Russian: Congratulations, you passed the test.";
-    private static final String EXAM_FAIL_RU = "Russian: You fail the test today, but you can retake it later.";
+    private static final String EXAM_ASK_NAME_RU = "Русский: what is your name?";
+    private static final String EXAM_WELCOME_RU = "Русский: Welcome to the chemistry test";
+    private static final String EXAM_PASS_RU = "Русский: Congratulations, you passed the test.";
+    private static final String EXAM_FAIL_RU = "Русский: You fail the test today, but you can retake it later.";
 
     @DisplayName("getExamPropertiesValue should get correct i18n values for ru_RU locale:")
     @Test
@@ -56,8 +57,10 @@ class UtilTest {
     @Test
     void getExamPropertiesValueForEngLocale() {
         //util.setLocale(Locale.ENGLISH);
-        examConfig.setLocaleLanguage("en");
-        examConfig.setLocaleCountry("US");
+        /*examConfig.setLocaleLanguage("en");
+        examConfig.setLocaleCountry("US");*/
+        localization.setLanguage("ru");
+        localization.setCountry("US");
         assertEquals(localization.getExamPropertiesValue("exam.ask-name", null), EXAM_ASK_NAME_EN);
         assertEquals(localization.getExamPropertiesValue("exam.welcome" , null), EXAM_WELCOME_EN);
         assertEquals(localization.getExamPropertiesValue("exam.pass",     null), EXAM_PASS_EN);
