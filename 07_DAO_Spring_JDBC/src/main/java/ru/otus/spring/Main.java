5ce3b241f1c562e811aea960c0335cd0c23891b9
Alex4790354 <9720560@gmail.com>
2022-01-07 00:15:50 +0300

    Remove comments

--- 07_DAO_Spring_JDBC/src/main/java/ru/otus/spring/Main.java
+++ 07_DAO_Spring_JDBC/src/main/java/ru/otus/spring/Main.java
@@ -14,47 +14,5 @@ public class Main {
 
         ApplicationContext context = SpringApplication.run(Main.class);
 
-        /*
-        AuthorDao authorDao = context.getBean(AuthorDao.class);
-        for (Author author : authorDao.getAll()) {
-            System.out.println(author.getName());
-        }
-
-        System.out.println(System.lineSeparator() + "Genre: ");
-        GenreDao genreDao = context.getBean(GenreDao.class);
-        for (Genre genre : genreDao.getAll()) {
-            System.out.println(genre.getName());
-        }
-
-
-        System.out.println(System.lineSeparator() + "Books: ");
-        BookDao bookDao = context.getBean(BookDao.class);
-
-        System.out.println("Book with id=1: " + bookDao.getById(1));
-        System.out.println("Total books: " + bookDao.getCount());
-
-
-        bookDao.deleteById(1);
-        System.out.println("Total books now: " + bookDao.getCount());
-
-        bookDao.updateById(3, "NO NO NO");
-        System.out.println("Book with id=3: " + bookDao.getById(3) + System.lineSeparator());
-
-        Author author = new Author(4, "Аркадий и Борис Стругацкие");
-        Genre genre = new Genre(3, "Фантастика");
-        Book newBook = new Book(author, genre, "Отель у погибшего альпениста 2");
-        System.out.println("Insert book: " + newBook + System.lineSeparator());
-        bookDao.insert(newBook);
-
-        author = new Author(10, "Нат такого автора");
-        genre = new Genre(10, "Нет такого жанра");
-        newBook = new Book(author, genre, "Попытка внести эту книгу должна вызвать ошибку");
-        System.out.println("Insert book-2: " + newBook + System.lineSeparator());
-        bookDao.insert(newBook);
-        for (Book book : bookDao.getAll()) {
-            System.out.println(book.getName());
-        }
-        System.out.println(System.lineSeparator()); */
-
     }
 }
